1c4a8581d1273f52b2331c0541f03ef0b695e90f
uUtkarsh3vedi <131517100+uUtkarsh3vedi@users.noreply.github.com>
2023-07-28 15:43:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+
+//components
+import NavBar from './components/NavBar';
+import Code from './components/Code';
+import AllUsers from './components/AllUsers';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+     <NavBar />
+     <Routes>
+      <Route path='/'element={<Code />} />
+      <Route path='/all'element={<AllUsers/>} />
+      <Route path='/add'element={<AddUser />} />
+      <Route path='/edit/:id'element={<EditUser />} />
+      </Routes>
+      </BrowserRouter>
+      
+  );
+}
+
+export default App;
